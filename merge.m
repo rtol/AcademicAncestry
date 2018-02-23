5ee31019147bc00591e29b40eedf792d31b96687
Richard S.J. Tol <r.tol@sussex.ac.uk>
2018-02-23 14:27:18 +0000

    Add files via upload

---
+++ merge.m
@@ -0,0 +1,4 @@
+G1 = getactree('59043');
+G2 = getactree('747092');
+G = mergedigraphs(G1,G2);
+plot(G)
---
+++ mergedigraphs.m
@@ -0,0 +1,23 @@
+function G = mergedigraphs(G1,G2)
+
+T = [G1.Edges; G2.Edges];
+Tr = T(1,1);
+for i=2:size(T,1)
+    c1 = table2cell(T(i,1));
+    s1 = char(c1{1,1}{1});
+    s2 = char(c1{1,1}{2});
+    add = true;
+    for j=1:i-1
+        c2 = table2cell(T(j,1));
+        s3 = char(c2{1,1}{1});
+        s4 = char(c2{1,1}{2});
+        add = add & ~(strcmp(s1,s3) & strcmp(s2,s4));
+    end
+    if add
+        Tr = [Tr;T(i,1)];
+    end
+end
+
+G = digraph(Tr);
+
+end
